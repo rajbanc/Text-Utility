3456cf5f90da73cb01dc49322701f6582044f5e1
rajbanc <dev.ai2@ramlaxmangroup.com>
2023-03-23 09:54:07 +0545

    first commit

---
+++ myapp/urls.py
@@ -0,0 +1,51 @@
+from django.urls import path, include
+from . import views
+from .views import *
+from django.views.generic.base import TemplateView
+from django.contrib.auth import views as auth_views
+
+urlpatterns = [
+    path('index',views.index, name='index'),
+    path('analyze',views.analyze, name='analyze'),
+    path('register/',views.register, name='register'),
+    path('login/', views.login_user, name='login'),
+    path('logout/', views.logout_user, name='logout'),
+    # path('account/',include("django.contrib.auth.urls")),
+    path('',views.home, name = "home"),
+    # path('',TemplateView.as_view(template_name = 'home.html')),
+    path('edit_profile/', views.edit_profile, name='edit_profile'),
+    path('change_password/', views.change_password, name='change_password'),
+
+    path('password_reset/', auth_views.PasswordResetView.as_view(),
+         name='password_reset'),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(),
+         name='password_reset_done'),
+    path('password_reset/confirm/<uidb64>/<token>/',
+         auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('password_reset/complete/', auth_views.PasswordResetCompleteView.as_view(),
+         name='password_reset_complete'),
+     path('password-reset/',
+         auth_views.PasswordResetView.as_view(
+             template_name='commons/password-reset/password_reset.html',
+             subject_template_name='commons/password-reset/password_reset_subject.txt',
+             email_template_name='commons/password-reset/password_reset_email.html',
+             # success_url='/login/'
+                ),
+         name='password_reset'),
+    path('password-reset/done/',
+         auth_views.PasswordResetDoneView.as_view(
+             template_name='password_reset_done.html'
+         ),
+         name='password_reset_done'),
+    path('password-reset-confirm/<uidb64>/<token>/',
+         auth_views.PasswordResetConfirmView.as_view(
+             template_name='commons/password-reset/password_reset_confirm.html'
+         ),
+         name='password_reset_confirm'),
+    path('password-reset-complete/',
+         auth_views.PasswordResetCompleteView.as_view(
+             template_name='commons/password-reset/password_reset_complete.html'
+         ),
+         name='password_reset_complete'),
+    ]         
+ 
---
+++ myapp/views.py
@@ -0,0 +1,164 @@
+from django.http import HttpResponse
+from django.shortcuts import render,redirect
+from django.contrib.auth import login, logout, authenticate, update_session_auth_hash
+from django.contrib.auth import forms
+from django.contrib import messages
+from django.contrib.auth.forms import UserCreationForm
+from .forms import  EditProfileForm,ChangePasswordForm
+
+# Create your views here.
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(request, username=username, password=password)
+            login(request, user)
+            return redirect('home')
+        else:
+            form = UserCreationForm(request.POST)
+    else:
+        form = UserCreationForm()
+    context = {
+        'form': form,
+    }
+    return render(request, 'registration/register.html', context)
+
+def login_user(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            if user.is_superuser:
+                print("hello")
+                messages.warning(request,"User is Superuser")
+                return redirect('/admin')
+        
+            elif user.is_active:
+                login(request, user)
+                print("hello1")
+                return redirect('/index',messages.info(request, 'You have been logged in successfully'))
+            
+            elif user is not authenticate:
+                login(request, user)
+                return redirect("registration/register.html", messages.success(request,"You must register first!"))
+        
+        else:
+            messages.warning(request, "Username or Password is incorrect !!")
+            return redirect('/login')
+    
+    else:
+        return render(request, 'registration/login.html')
+
+def logout_user(request):
+    logout(request)
+    messages.info(request, "Logged out successfully")
+    return redirect('home')
+
+def edit_profile(request):
+    if request.method == 'POST':
+        form = EditProfileForm(request.POST, instance=request.user)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Profile Updated Successfully")
+            return redirect('home')
+    else:
+        form = EditProfileForm(instance=request.user)
+    context = {
+        'form': form,
+    }
+    return render(request, 'authenticate/edit_profile.html', context)
+
+def change_password(request):
+    if request.method == 'POST':
+        form = ChangePasswordForm(data=request.POST, user=request.user)
+        if form.is_valid():
+            form.save()
+            update_session_auth_hash(request, form.user)
+            messages.success(request, "Password Changed Successfully")
+            return redirect('home')
+    else:
+        form = ChangePasswordForm(user=request.user)
+        print(form)
+    context = {
+        'form': form,
+    }
+    return render(request, 'authenticate/change_password.html', context)
+
+
+def home(request):
+    return render(request,'base.html')
+def index(request):
+    return render(request,'index.html')
+   
+def analyze(request):
+    djtext = request.POST.get('text','default')
+   
+    
+    # Check checkbox values
+    removepunc = request.POST.get('removepunc', 'off')
+    fullcaps = request.POST.get('fullcaps', 'off')
+    newlineremover = request.POST.get('newlineremover', 'off')
+    extraspaceremover = request.POST.get('extraspaceremover', 'off')
+    capitalized = request.POST.get('capitalized','off')
+    charcount = request.POST.get('charcount','off')
+
+    #Check which checkbox is on
+    if removepunc == "on":
+        punctuations = '''[{!()-]};:'"\,<>./?@#$%^&*_~'''
+        analyzed = ""
+        for char in djtext:
+            if char not in punctuations:
+                analyzed = analyzed + char
+        params = {'purpose':'Removed Punctuations', 'analyzed_text': analyzed}
+        djtext=analyzed
+        
+    if fullcaps =="on":
+        analyzed=""
+        for char in djtext:
+            analyzed=analyzed+char.upper()
+        params = {'purpose':'All Character are Uppercase', 'analyzed_text': analyzed}
+        djtext=analyzed
+    
+    if(extraspaceremover=="on"):
+        analyzed = ""
+        for index, char in enumerate(djtext):
+            if not(djtext[index] == " " and djtext[index+1]==" "):
+                analyzed = analyzed + char
+
+        params = {'purpose': 'Removed NewLines', 'analyzed_text': analyzed}
+        djtext=analyzed
+    
+    if (newlineremover == "on"):
+        analyzed = ""
+        for char in djtext:
+            if char != "\n" and char!="\r":
+                analyzed = analyzed + char
+
+        params = {'purpose': 'Removed NewLines', 'analyzed_text': analyzed}
+        djtext=analyzed
+
+    if (capitalized == "on"):
+        analyzed = ""
+        for char in djtext:
+            analyzed=djtext.title()
+
+        params = {'purpose': 'capitalized', 'analyzed_text': analyzed}
+        djtext=analyzed
+    
+    if (charcount == "on"):
+        analyzed = ""
+        for char in djtext:
+            analyzed=len(djtext)
+
+        params = {'purpose': 'charcount', 'analyzed_text': analyzed}
+        djtext = analyzed
+    
+    if(removepunc != "on" and newlineremover!="on" and extraspaceremover!="on" and fullcaps!="on" and capitalized!="on" and charcount!="on"):
+        return HttpResponse("please select any operation and try again")
+
+    return render(request, 'analyze.html', params)
